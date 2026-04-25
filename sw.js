5dc194178ccee6d060a1e7b65e477ee90a84dfec
klaus-br <147872140+klaus-br@users.noreply.github.com>
2026-04-25 20:14:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
-const CACHE = 'womo-v1';
+const CACHE = 'womo-v3'; // ← Versionsnummer hier hochzählen bei Updates
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  // Alle alten Cache-Versionen löschen
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
