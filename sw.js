a6acd05b76d473e33eec1a1ac10ddbfb1f389204
klaus-br <147872140+klaus-br@users.noreply.github.com>
2026-04-27 12:56:46 +0200

    Update cache version from v7 to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'womo-v7'; // ← Versionsnummer hier hochzählen bei Updates
+const CACHE = 'womo-v8'; // ← Versionsnummer hier hochzählen bei Updates
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
